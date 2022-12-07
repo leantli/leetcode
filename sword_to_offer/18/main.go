93f0c19ca98dc99858787f3f0ad280962eba597f
LeantLi <117699758+leantli@users.noreply.github.com>
2022-12-07 16:31:50 +0800

    the 11th day of sword to offer (#2)

---
+++ sword_to_offer/18/main.go
@@ -0,0 +1,35 @@
+package main
+
+// https://leetcode.cn/problems/shan-chu-lian-biao-de-jie-dian-lcof/?envType=study-plan&id=lcof&plan=lcof&plan_progress=131d40r
+// 18. 删除链表的节点
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// 先试了一下递归，感觉递归不如直接迭代好处理，还是直接迭代完事，最后特殊处理一下头节点就ok
+
+func deleteNode(head *ListNode, val int) *ListNode {
+	if head.Val == val {
+		return head.Next
+	}
+	l, r := head, head
+	for r != nil {
+		r = r.Next
+		if r.Val == val {
+			l.Next = r.Next
+			break
+		}
+		l = l.Next
+	}
+	return head
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
