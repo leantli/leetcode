2fea352f7263bd6303d47becb757b3036ba0e444
leantli <leantli@qq.com>
2022-12-07 18:29:32 +0800

    the 13th day of sword to offer

---
+++ sword_to_offer/21/main.go
@@ -0,0 +1,72 @@
+package main
+
+// https://leetcode.cn/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/?envType=study-plan&id=lcof&plan=lcof&plan_progress=131d40r
+// 21. 调整数组顺序使奇数位于偶数前面
+
+// 马上想到两次遍历取奇偶，一个数组 append，不过这样应该不是最优解，应该有一次遍历解决的操作？
+
+// func exchange(nums []int) []int {
+// 	res := make([]int, 0, len(nums))
+// 	for _, num := range nums {
+// 		if (num & 1) == 1 {
+// 			res = append(res, num)
+// 		}
+// 	}
+// 	for _, num := range nums {
+// 		if (num & 1) == 0 {
+// 			res = append(res, num)
+// 		}
+// 	}
+// 	return res
+// }
+
+// // 第一次优化，一次遍历，但用两个数组
+// func exchange(nums []int) []int {
+// 	res := make([]int, 0, len(nums))
+// 	// 第一次优化，用两个数组
+// 	another := make([]int, 0)
+// 	for _, num := range nums {
+// 		if (num & 1) == 1 {
+// 			res = append(res, num)
+// 		} else {
+// 			another = append(another, num)
+// 		}
+// 	}
+// 	res = append(res, another...)
+// 	return res
+// }
+
+// // 第二次优化，一个数组+一次遍历
+// func exchange(nums []int) []int {
+// 	n := len(nums)
+// 	res := make([]int, n, n)
+// 	l, r := 0, n-1
+// 	for _, num := range nums {
+// 		if (num & 1) == 1 {
+// 			res[l] = num
+// 			l++
+// 		} else {
+// 			res[r] = num
+// 			r--
+// 		}
+// 	}
+// 	return res
+// }
+
+// 双指针+原地修改解
+func exchange(nums []int) []int {
+	n := len(nums)
+	l, r := 0, n-1
+	for l < r {
+		if (nums[l] & 1) == 1 {
+			l++
+		}
+		if (nums[r] & 1) != 1 {
+			r--
+		}
+		if l < r {
+			nums[l], nums[r] = nums[r], nums[l]
+		}
+	}
+	return nums
+}
