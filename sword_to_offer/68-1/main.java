1ced714bdba44afe10d7c09bf2ddddfdf7c0d192
leantli <leantli@qq.com>
2022-12-13 18:33:10 +0800

    the 20th day of sword to offer

--- sword_to_offer/68-1/main.java
+++ sword_to_offer/68-1/main.java
@@ -10,6 +10,7 @@ class Solution {
         if (root == null) return null;
         if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
         if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        // 不符合上面两个 if 的话，就说明是一个大一个小，当前 root 已经是其最近公共祖先了
         return root;
     }
 }
